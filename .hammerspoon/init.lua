ebf833e2c90bc09bc0dfcfd4d12969eaf84baeed
Hannes Kinnunen <20144380+Steellow@users.noreply.github.com>
2022-02-12 10:45:19 +0200

    Update init.lua

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -7,7 +7,7 @@ local function pressFn(mods, key)
 	return function() hs.eventtap.keyStroke(mods, key, 1000) end
 end
 
-local function remap(mods, key, pressFn
+local function remap(mods, key, pressFn)
 	hs.hotkey.bind(mods, key, pressFn, nil, pressFn)	
 end
 
